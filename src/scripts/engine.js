c6677f54572459320a393038386c615e9dcac492
Matthews Bastos <matthewsbasttos@gmail.com>
2023-11-02 16:55:39 -0300

    feat: Creates state object and capture view elements

---
+++ src/scripts/engine.js
@@ -0,0 +1,9 @@
+const state = {
+  view: {
+    timeLeft: document.querySelector("#time-left"),
+    score: document.querySelector("#score"),
+    squares: document.querySelectorAll(".square"),
+    enemy: document.querySelector(".enemy"),
+  },
+  values: {},
+};
